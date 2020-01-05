ccfeeb431f8071a452df99feb391fb177838d591
Julian McNeill <julesdowork@gmail.com>
2020-01-04 21:35:47 -0500

    Fix collision detection

--- Addblox/Assets/Scripts/Block.cs
+++ Addblox/Assets/Scripts/Block.cs
@@ -2,53 +2,48 @@
 
 public class Block : MonoBehaviour
 {
+    private enum State { FALLING, RESTING };
+
     [SerializeField]
     private float speed = 50f;
-    private float fasterSpeed;
-    private bool isFalling = true;
+    private State currentState = State.FALLING;
     private float offset = 1f;
 
     private float minX = -4f, maxX = 4f;
-
-    private Rigidbody2D rb;
-
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-
-        fasterSpeed = speed * 5f;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb.velocity = new Vector2(0, -speed * Time.deltaTime);
-    }
+    [SerializeField]
+    private LayerMask blockLayer;
+    private Vector3 rayOffset = new Vector3(0.5f, 0, 0);
 
     // Update is called once per frame
     void Update()
     {
-        if (isFalling)
+        if (currentState == State.FALLING)
+        {
+            Fall();
             Move();
-        else
-            rb.velocity = Vector3.zero;
-
-        FallFaster();
+            Drop();
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("BottomBounds") ||
-            collision.gameObject.CompareTag("Block"))
+            (collision.gameObject.CompareTag("Block") && currentState == State.FALLING))
         {
-            isFalling = false;
+            currentState = State.RESTING;
             BlockSpawner.instance.SpawnNewBlock();
         }
     }
 
-    void Move()
+    private void Fall()
     {
-        float newX = rb.position.x;
+        //Debug.DrawRay(transform.localPosition + rayOffset, Vector2.down * 8f, Color.red);
+        transform.Translate(-transform.up * speed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        float newX = transform.position.x;
 
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
             newX -= offset;
@@ -56,15 +51,27 @@ public class Block : MonoBehaviour
             newX += offset;
 
         float clampedX = Mathf.Clamp(newX, minX, maxX);
-        rb.position = new Vector2(clampedX, rb.position.y);
+        transform.position = new Vector3(clampedX, transform.position.y, 0);
     }
 
-    void FallFaster()
+    private void Drop()
     {
-        if (Input.GetKey(KeyCode.DownArrow))
-            rb.velocity = new Vector2(0, -fasterSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            float yPos = FindNextAvailableYPosition();
+            transform.position = new Vector3(transform.position.x, yPos, 0);
+        }
+    }
 
-        if (Input.GetKeyUp(KeyCode.DownArrow))
-            rb.velocity = new Vector2(0, -speed * Time.deltaTime);
+    private float FindNextAvailableYPosition()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + rayOffset, Vector2.down, 8f, blockLayer);
+
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject.transform.position.y + 0.5f;
+        }
+
+        return 0;
     }
 }
