72aaadb1cd73dc02a37006c98b68a55da645214b
Julian McNeill <julesdowork@gmail.com>
2020-01-10 21:51:32 -0500

    Taking Time

---
+++ Addblox/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private int totalTime;
+    private int seconds;
+    private int minutes;
+    private string time;
+    private TextMeshProUGUI timeText;
+
+    void Awake()
+    {
+        timeText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CountUpTime", 0.1f, 1f);
+    }
+
+    void CountUpTime()
+    {
+        totalTime = (int)Time.time;
+        minutes = totalTime / 60;
+        seconds = totalTime % 60;
+        time = minutes.ToString("D2") + ":" + seconds.ToString("D2");
+        timeText.text = time;
+    }
+}
